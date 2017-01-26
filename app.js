df9c3fb9d8f4afcc9303ad1424697748f990064e
Serkan Aysan <serkan.aysan@outlook.com>
2017-01-26 11:19:37 +0300

    post method

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -10,6 +14,11 @@ app.get('/', function(req, res){
 
 app.get('/contact', function(req, res){
   res.render('contact',{qs: req.query} );
+});
+
+app.post('/contact', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function(req, res){
